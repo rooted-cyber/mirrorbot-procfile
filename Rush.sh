0d54e0fa2c82585bfee39895a7f7f78d61706b11
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-05-19 14:19:56 +0530

    Add files via upload

--- Rush.sh
+++ Rush.sh
@@ -1,3 +1,8 @@
-wget https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/reqs.sh
-bash reqs.sh
-apt-get install qbittorrent-nox -y
+apt-get update -y;apt-get install -y figlet toilet neofetch apt-utils sudo wget
+figlet hi; neofetch
+cd /usr/share/figlet;wget https://raw.githubusercontent.com/rooted-cyber/uploading/main/font.flf
+figlet -f font Install
+cd /root/.config/neofetch;rm config.conf;wget https://raw.githubusercontent.com/rooted-cyber/uploading/main/c1/config.conf
+neofetch;pip install --upgrade pip;pip3 install youtube-dl;neofetch
+wget -O do.sh https://get.docker.com/;bash do.sh
+wget -O dep.sh https://raw.githubusercontent.com/rooted-cyber/okteto-bot/main/sppt.sh;bash dep.sh
