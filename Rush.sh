57f5d019695d6158a271e691aea47605645bcfad
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-05-25 05:52:22 +0530

    Update Rush.sh

--- Rush.sh
+++ Rush.sh
@@ -1,8 +1,1 @@
-apt-get update -y;apt-get install -y figlet toilet neofetch apt-utils sudo wget
-figlet hi; neofetch
-cd /usr/share/figlet;wget https://raw.githubusercontent.com/rooted-cyber/uploading/main/font.flf
-figlet -f font Install
-cd /app/.config/neofetch;rm config.conf;wget https://raw.githubusercontent.com/rooted-cyber/uploading/main/c1/config.conf
-neofetch;pip install --upgrade pip;pip3 install youtube-dl;neofetch
-wget -O do.sh https://get.docker.com/;bash do.sh
-wget -O dep.sh https://raw.githubusercontent.com/rooted-cyber/all-sh-bot/main/sppt.sh;bash dep.sh
+neofetch
