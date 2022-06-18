2246811aa3ac11f6c2d9dcbed2cfc16264edf563
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-06-18 20:24:05 +0530

    Update Rush.sh

--- Rush.sh
+++ Rush.sh
@@ -1,8 +1,7 @@
 python3 -m pyultroid
 git clone https://github.com/TeamUltroid/Ultroid
 cd Ultroid
-bash startup
-pip install pyUltroid
-pip install py-Ultroid
-bash startup
-npm
+ls
+ls -a
+ls --help
+apt list
